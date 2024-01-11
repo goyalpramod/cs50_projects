8e75a40fa6d5d0eaf7a097eef2fe044a3b3c4d4e
goyalpramod <goyalpramod1729@gmail.com>
2024-01-11 17:55:40 +0530

    completing entry page task

--- project_1/wiki/encyclopedia/views.py
+++ project_1/wiki/encyclopedia/views.py
@@ -8,3 +8,8 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    return render(request, "encyclopedia/entry.html", {
+        "entry": util.get_entry(title),
+        "title": title
+    })
