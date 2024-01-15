d5758275e28697555dab68f4f01c47715f0404df
goyalpramod <goyalpramod1729@gmail.com>
2024-01-15 14:14:17 +0530

    Add search functionality to encyclopedia views.py

--- project_1/wiki/encyclopedia/views.py
+++ project_1/wiki/encyclopedia/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 from . import util
 
@@ -13,3 +13,17 @@ def entry(request, title):
         "entry": util.get_entry(title),
         "title": title
     })
+
+def search(request):
+       query = request.GET.get('q', '')
+       all_entries = util.list_entries()
+       
+       if query in all_entries:
+           return redirect('entry', title=query)
+       else:
+           # Filter entries that contain the query as a substring
+           search_results = [entry for entry in all_entries if query.lower() in entry.lower()]
+           return render(request, 'encyclopedia/search_results.html', {
+               'search_results': search_results,
+               'query': query
+           })
