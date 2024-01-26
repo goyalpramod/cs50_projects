9eb5957edd2dce6453658093f86ff7daf954964e
goyalpramod <goyalpramod1729@gmail.com>
2024-01-26 19:04:57 +0530

    Add edit_page URL pattern

--- project_1/wiki/encyclopedia/urls.py
+++ project_1/wiki/encyclopedia/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("search/", views.search, name="search"),
     path("random/", views.random_page, name="random_page"),
     path("new/", views.new_page, name="new_page"),
+    path("edit/<str:title>", views.edit_page, name="edit_page"),
 ]
