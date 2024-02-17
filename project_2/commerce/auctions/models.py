f6b79a3baebfb3383593412ab25abdcba2fa6b31
goyalpramod <goyalpramod1729@gmail.com>
2024-02-17 21:39:58 +0530

    Add User name and email fields, create Comments, AuctionListing, and Bids models

--- project_2/commerce/auctions/models.py
+++ project_2/commerce/auctions/models.py
@@ -3,4 +3,28 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    name = models.CharField(max_length=60)
+    email = models.EmailField()
+    #TODO password?
+
+class Comments(models.Model):
+    name = models.CharField(max_length=60)
+    date = models.DateTimeField()
+    comment = models.CharField(max_length=200)
+
+class AuctionListing(models.Model):
+    image = models.ImageField(upload_to='images/')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    title = models.CharField(max_length=60)
+    description = models.CharField(max_length=200)
+    category = models.CharField(max_length=60)
+    date = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comments = models.ManyToManyField(Comments)
+
+
+class Bids(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    auction = models.ForeignKey(AuctionListing, on_delete=models.CASCADE)
+    bid = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField()
